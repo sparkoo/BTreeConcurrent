7456f0c4a7eabcbc0ad07fb0b2491e1c129e4779
Michal Vala <michal.vala@gmail.com>
2014-05-30 22:00:33 +0200

    napraseny

--- src/BTree.java
+++ src/BTree.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class BTree<Item extends Comparable> {
     private Node root;
@@ -30,51 +28,159 @@ public class BTree<Item extends Comparable> {
         if (node.getRightChild() != null)
             infix(node.getRightChild(), sorted);
     }
-	
+
+    private void infix(Node node) {
+        if (node == null) return;
+        if (node.getLeftChild() != null)
+            infix(node.getLeftChild());
+        System.out.print(node.getItem() + " ");
+        if (node.getRightChild() != null)
+            infix(node.getRightChild());
+    }
+
+    private void prefix(Node node) {
+        if (node == null) return;
+        System.out.print(node.getItem() + " ");
+        if (node.getLeftChild() != null)
+            prefix(node.getLeftChild());
+        if (node.getRightChild() != null)
+            prefix(node.getRightChild());
+    }
+
+    private void infixP(Node node, ArrayList<Node> boundaries, ArrayList<Node> threadRoots) {
+        if (node.getLeftChild() != null && node.getRightChild() != null) {
+            infixP(node.getLeftChild(), boundaries, threadRoots);
+            boundaries.add(node);
+            infixP(node.getRightChild(), boundaries, threadRoots);
+        } else {
+            threadRoots.add(node);
+        }
+    }
 			
-	public void addConcurrent(Item[] item) {
-		root = new Node(item[0]);
-		final Queue<Item> queue1 = new LinkedBlockingDeque<Item>();
-		final Queue<Item> queue2 = new LinkedBlockingDeque<Item>();
-		for (int i = 1; i < item.length; i++) {
-			if (item[i].compareTo(root.getItem()) > 0) {
-				queue1.add(item[i]);
-			} else {
-				queue2.add(item[i]);
-			}
-		}
-		Thread t1 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				long start;
-				start = System.currentTimeMillis();
-				for (Item i : queue2) {
-					if (root.leftChild != null)
-						root.leftChild.addItem(i);
-					else
-						root.leftChild = new Node(i);
-				}
-				System.out.println("Thread 1 time: " + (System.currentTimeMillis() - start) / 1000.0 + "s");
-			}
-		});
-		
-		Thread t2 = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				long start;
-				start = System.currentTimeMillis();
-				for (Item i : queue1) {
-					if (root.rightChild != null)
-						root.rightChild.addItem(i);
-					else
-						root.rightChild = new Node(i);
-				}
-				System.out.println("Thread 2 time: " + (System.currentTimeMillis() - start) / 1000.0 + "s");
-			}
-		});
-		t1.start();
-		t2.start();
+	public void addConcurrent(Item[] item, int threads) {
+        final long start;
+        start = System.currentTimeMillis();
+        int count = threads * 2 - 1;
+        sortN(item, count);
+        root = new Node(item[count / 2]);
+        prepareTree(item, root, count / 2, count / 2);
+        /*for (Item i : item) {
+            System.out.print(i + " ");
+        }
+        System.out.println();*/
+        ArrayList<Node> boundaries = new ArrayList<Node>();
+        ArrayList<Node> threadRoots = new ArrayList<Node>();
+        infixP(root, boundaries, threadRoots);
+        /*
+        for (Node n : boundaries) {
+            System.out.print(n.getItem() + "; ");
+        }
+
+        System.out.println();
+        prefix(root);
+        System.out.println();
+
+        for (Node n : threadRoots) {
+            System.out.print(n.getItem() + " > ");
+        }
+        System.out.println();
+*/
+        ArrayList<Queue> queues = new ArrayList<Queue>();
+        ArrayList<Thread> threadList = new ArrayList<Thread>();
+        for (int i = 0; i < threads; i++) {
+            Queue<Item> q = new LinkedBlockingDeque<Item>();
+            queues.add(q);
+            Thread t = new BTreeAddingThread(threadRoots.get(i), q);
+            threadList.add(t);
+            t.start();
+        }
+
+        for (int i = count; i < item.length; i++) {
+            int bIndex = boundaries.size();
+            for (int boundaryIndex = 0; boundaryIndex < boundaries.size(); boundaryIndex++) {
+                if (item[i].compareTo(boundaries.get(boundaryIndex).getItem()) < 0) {
+                    bIndex = boundaryIndex;
+                    break;
+                }
+            }
+            queues.get(bIndex).add(item[i]);
+        }
+
+        while (true) {
+            boolean isAllEmpty = true;
+            for (Queue q : queues) {
+                if (!q.isEmpty()) {
+                    isAllEmpty = false;
+                    break;
+                }
+            }
+            if (isAllEmpty) {
+                break;
+            } else {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        for (Thread t : threadList)
+            t.interrupt();
+        System.out.println("concurrent time: " + (System.currentTimeMillis() - start) / 1000.0 + "s");
 	}
+
+    private void prepareTree(Item[] item, Node parent, int count, int index) {
+        try {
+            if (count > 0) {
+                int c = count / 2 + 1;
+                parent.addItem(item[index - c]);
+                parent.addItem(item[index + c]);
+                prepareTree(item, parent.leftChild, count / 2, index - c);
+                prepareTree(item, parent.rightChild, count / 2, index + c);
+            }
+        } catch (Exception e) {
+            System.out.println("count: " + count + "; index: " + index + "; item: " + parent.getItem());
+        }
+    }
+
+    private void sortN(Item[] item, int count) {
+        Item p;
+        int min;
+        for (int i = 0; i < count; i++) {
+            min = i;
+            for (int j = i + 1; j < count; j++)
+                if (item[j].compareTo(item[min]) < 0)
+                    min = j;
+            p = item[i];
+            item[i] = item[min];
+            item[min] = p;
+        }
+    }
+
+    private class BTreeAddingThread extends Thread {
+        private Node root;
+        private Queue<Item> queue;
+
+        public BTreeAddingThread(Node root, Queue<Item> queue) {
+            this.root = root;
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                if (queue.isEmpty()) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                } else {
+                    root.addItem(queue.remove());
+                }
+            }
+        }
+    }
 
     private class Node {
         private Item item;
@@ -119,18 +225,19 @@ public class BTree<Item extends Comparable> {
             array[i] = r.nextInt(size * 10);
         }
 
+        bTree2.addConcurrent(array, 16);
+        System.out.println(isSorted(bTree2.getSorted()) + " ==> " + bTree2.getSorted().size());
+
         long start;
         start = System.currentTimeMillis();
 
 		for (int i = 0; i < size; i++) {
 			bTree.addItem(array[i]);
 		}
-        System.out.println((System.currentTimeMillis() - start) / 1000.0 + "s");
-		start = System.currentTimeMillis();
-		bTree2.addConcurrent(array);
+        System.out.println("normal time: " + (System.currentTimeMillis() - start) / 1000.0 + "s");
     }
 	
-	public boolean isSorted(ArrayList<Item> list) {
+	public static boolean isSorted(ArrayList<Integer> list) {
 		boolean sorted = true;        
 		for (int i = 1; i < list.size(); i++) {
 			if (list.get(i-1).compareTo(list.get(i)) > 0) sorted = false;
